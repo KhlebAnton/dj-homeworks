3f8e014f8c38af384912f13727e5950542819b1e
Anton Khlebnikov <toni.farsh@icloud.com>
2024-10-08 15:26:52 +0500

    git init

---
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+}
+
+
+def recipes_view(request, dish_name):
+    servings = int(request.GET.get('servings', 1))
+    dish_dict = {}
+    for dish, ingredients in DATA.items():
+        if dish_name == dish:
+            for ingredient, count in ingredients.items():
+                dish_dict[ingredient] = count * servings
+    context = {
+        'recipe': dish_dict,
+        'title': dish_name,
+        'servings': servings
+    }
+    return render(request, 'calculator/index.html', context)
